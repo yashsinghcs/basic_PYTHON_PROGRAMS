d572d82b4e11f5948e202b5fe83ff966087ed64a
YASH KUMAR SINGH <yash.singh_cs18@gla.ac.in>
2019-08-24 01:42:25 -0700

    added if-else statement and printed desired output

--- positive_or_negative_number/TestPositiveOrNegative.py
+++ positive_or_negative_number/TestPositiveOrNegative.py
@@ -1,3 +1,9 @@
-#@author=Yash Kumar Singh
-#@version 1.0
-value =eval(input("Enter the number to be checked"))
+# @author=Yash Kumar Singh
+# @version 1.0
+value = eval(input("Enter the number to be checked"))
+if value > 0:
+    print("positive")
+elif value < 0:
+    print("negative")
+else:
+    print("Zero")
